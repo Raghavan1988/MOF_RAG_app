9a7811564aeec7c318ebdd1b4f7276c810169c82
Raghavan <raghavan.mit@gmail.com>
2024-05-22 22:50:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def get_model_info(hf_url):
     # Get answers to the questions using the Perplexity API
     answers = {}
     answers["model"]  = model
+    url = hf_url
     answers["q1"] = get_pplxity_response(dataset, model, url)
     answers["q2"] = get_pplxity_response(model_weights, model, url)
     answers["q3"] = get_pplxity_response(inference_code, model, url)
